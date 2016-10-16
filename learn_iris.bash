c37c79cd6fbbbc0c495a9d35c44939bf709e253d
Dan Bikle <bikle@bikle.com>
2016-10-15 18:02:38 -0700

    more

---
+++ learn_iris.bash
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# learn_iris.bash
+
+# This script should learn from iris.csv and then create files which describe a model.
+# Later, that model should be served to browsers.
+# Then, the browsers should use the model to make predictions.
+
+keras_theano.bash learn_iris.py
+
+exit
