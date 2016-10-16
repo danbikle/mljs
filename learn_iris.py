39226119b4204dcc63c1681314e782c13edd0a5b
Dan Bikle <bikle@bikle.com>
2016-10-15 19:06:39 -0700

    more

--- learn_iris.py
+++ learn_iris.py
@@ -15,7 +15,37 @@ import pdb
 
 iris_df = pd.read_csv('iris.csv', header=None, names=['f1','f2','f3','f4','iristype'])
 
+print('Here is some data I am learning from:')
 print(iris_df.head())
+
+xtrain_a = iris_df.values[:, 0:4]
+y = iris_df.values[:, 4]
+
+# I should one-hot-encode setosa, virginica, versicolor:
+ytrain_l = []
+for y_s in y:
+  if y_s == 'setosa':
+    ytrain_l.append([1,0,0])
+  elif y_s == 'virginica':
+    ytrain_l.append([0,1,0])
+  else:
+    ytrain_l.append([0,0,1])
+
+# I should use Keras API to create a neural network model:
+model = Sequential()
+model.add(Dense(4, input_shape=(4,)))
+model.add(Activation('relu'))
+model.add(Dense(3))
+model.add(Activation('softmax'))
+model.compile(loss='categorical_crossentropy', optimizer='adam')
+model.fit(xtrain_a, ytrain_l, batch_size=1, nb_epoch=50)
+
+# It should be able to predict now:
+print(model.predict(xtrain_a[5:8]), ytrain_l[5:8])
+print(model.predict(xtrain_a[55:58]), ytrain_l[55:58])
+print(model.predict(xtrain_a[105:108]), ytrain_l[105:108])
+# The above predictions should be very accurate.
+# Why? Because I am predicting 9 observations from training data.
 
 'bye'
 
