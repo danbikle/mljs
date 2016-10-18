2f3bf9151731cf03ff6e58c26d8a887139b25f35
Dan Bikle <bikle@bikle.com>
2016-10-17 23:09:03 -0700

    more

--- gspc1.js
+++ gspc1.js
@@ -1,5 +1,15 @@
 /*
 gspc1.js
 
+This script should read gspc2.csv into an array.
 */
 
+
+d3.csv("gspc2.csv", d3csv_cb)
+
+function d3csv_cb(err, datep_a){
+    // This callback should read gspc2.csv into an array: datep_a
+    if (err) throw err
+    datep_a
+    'bye'
+}
