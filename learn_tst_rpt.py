7cc2bdbc84436b54d105ea114261f5477437638d
Dan Bikle <bikle@bikle.com>
2016-10-18 21:12:11 -0700

    more

--- learn_tst_rpt.py
+++ learn_tst_rpt.py
@@ -43,11 +43,11 @@ train_df = feat_df[train_sr]
 test_df  = feat_df[test_sr]
 
 # I should get training data:
-x_train_a = np.array(train_df)[:,3:]
-y_train_a = np.array(train_df.pctlead)
+xtrain_a = np.array(train_df)[:,3:]
+ytrain_a = np.array(train_df.pctlead)
 
-# I should get classification from y_train_a:
-class_train_a = (y_train_a > np.mean(y_train_a))
+# I should get classification from ytrain_a:
+class_train_a   = (ytrain_a > np.mean(ytrain_a))
 class_train1h_l = [[0,1] if cl else [1,0] for cl in class_train_a]
 # [0,1] means up-observation
 # [1,0] means down-observation
@@ -55,4 +55,38 @@ ytrain1h_a = np.array(class_train1h_l)
 
 #
 # I should build a Keras model:
+from keras.models      import Sequential
+from keras.layers.core import Dense, Activation
+
+# I should use Keras API to create a neural network model.
+# Ref:
+# https://keras.io/getting-started/sequential-model-guide
+
+# I should look at the last observation to see number of inputs
+input_i = len(xtrain_a[-1])
+
+# I should look at the last observation to see number of outputs:
+
+print(ytrain1h_a[-1])
+output_i = len(ytrain1h_a[-1])
+# This is a classification model.
+# The number of outputs should be the number of classes I want to predict.
+# Usually for stockmarket, the number of classes is 2 (below-mean, above-mean).
+
+gspc_model = Sequential()
+gspc_model.add(Dense(input_i, input_shape=(input_i,)))
+gspc_model.add(Activation('relu'))
+gspc_model.add(Dense(output_i))
+gspc_model.add(Activation('softmax'))
+gspc_model.compile(loss='categorical_crossentropy', optimizer='adam')
+gspc_model.fit(xtrain_a, ytrain1h_a, batch_size=1, nb_epoch=11)
+
+# I should get test data:
+xtest_a = np.array(test_df)[:,3:]
+ytest_a = np.array(test_df.pctlead)
+
+# It should be able to predict now:
+predictions_a = gspc_model.predict(xtest_a)
+predictions_a[-10:]
+
 'bye'
